ef1184431b5385ce27924f59283518234f0d6007
Armin <a.mehran@gmail.com>
2025-12-04 20:34:32 -0500

    Replace LinkedIn badge with custom logo button

--- spec/linkedin_badge_spec.rb
+++ spec/linkedin_badge_spec.rb
@@ -5,15 +5,12 @@ describe 'LinkedIn Badge Integration' do
   let(:head_path) { '_includes/head/custom.html' }
   let(:config_path) { '_config.yml' }
 
-  it 'includes the LinkedIn badge div in the footer' do
-    footer_content = File.read(footer_path)
-    expect(footer_content).to include('class="badge-base LI-profile-badge"')
-    expect(footer_content).to include('data-vanity="arminmehran"')
-  end
 
-  it 'includes the LinkedIn profile script in the footer' do
+
+  it 'includes the custom LinkedIn button in the footer' do
     footer_content = File.read(footer_path)
-    expect(footer_content).to include('src="https://platform.linkedin.com/badges/js/profile.js"')
+    expect(footer_content).to include('class="btn btn--linkedin"')
+    expect(footer_content).to include('Connect on LinkedIn')
   end
 
   it 'has the correct LinkedIn URL in _config.yml' do
