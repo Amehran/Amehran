c5e70aac3ac7daa0500721b7275157ec79c9bf3b
Armin <a.mehran@gmail.com>
2025-12-04 21:06:45 -0500

    Enhance footer with aligned social buttons and centered copyright

--- spec/linkedin_badge_spec.rb
+++ spec/linkedin_badge_spec.rb
@@ -9,13 +9,13 @@ describe 'LinkedIn Badge Integration' do
 
   it 'includes the custom LinkedIn button in the footer' do
     footer_content = File.read(footer_path)
-    expect(footer_content).to include('class="btn btn--linkedin"')
     expect(footer_content).to include('Connect on LinkedIn')
+    expect(footer_content).to include('href="https://www.linkedin.com/in/arminmehran"')
   end
 
-  it 'has the correct LinkedIn URL in _config.yml' do
-    config = YAML.load_file(config_path)
-    linkedin_link = config['author']['links'].find { |l| l['label'] == 'LinkedIn' }
-    expect(linkedin_link['url']).to eq('https://www.linkedin.com/in/arminmehran')
+  it 'includes the button to the specific Medium article' do
+    footer_content = File.read(footer_path)
+    expect(footer_content).to include('Read on Medium')
+    expect(footer_content).to include('href="https://medium.com/@a.mehran/my-journey-from-silicon-constraints-to-intelligent-agents-3e2e3094ac88"')
   end
 end
